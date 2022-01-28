df546d645a3eda4ac50140b1e2539dc7243a2916
Zaidjayousi <56784268+Zaidjayousi@users.noreply.github.com>
2022-01-29 00:37:09 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'sa-east-1_wzkpdlOcg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4j3hf9ouu2thgs0jlk7h35tdbl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'sa-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_t48Rks3bd', // e.g. us-east-1_t48Rks3bd
+        userPoolClientId: '4b6ed8tdnsbnh7quu1c1us930c', // e.g. 4b6ed8tdnsbnh7quu1c1us930c
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: 'https://an2eirbqsk.execute-api.sa-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
